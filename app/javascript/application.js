2dee6b247ddc62aa3d43da3576ed8669614b5867
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-12-28 16:36:38 +0545

    Added postgresql config to use postgresql for the project

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,7 +6,7 @@ function App() {
   return (<h1>Hello World!</h1>);
 }
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App/>
 );
